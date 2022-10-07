bda24f8716a4f07db845cffa9db59f7758a9e281
tasrad <108927043+tasrad@users.noreply.github.com>
2022-10-07 17:44:27 +0300

    created responce

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const flovers = [{
-    image: '/img/cart1.png',
-    name: 'Ніжна свіжість: ',
-    desctiption: 'троянди та лілії',
-    cost: '445'
-}];
+// const flovers = [{
+//     image: '/img/cart1.png',
+//     name: 'Ніжна свіжість: ',
+//     desctiption: 'троянди та лілії',
+//     cost: '445'
+// }];
 
-function displayCardFlovers (){
+function displayCardFlovers (flovers){
     let contentFlovers = document.querySelector("#content");
     let floversContent = `<section id="content">`;
     flovers.forEach(function(flovers){
         floversContent = floversContent +
         `
         <div class="card-of-flowers">
- <div class="card-section4" >
-        <img src="url(${flovers.image})" width="310px" height="310px">
-       <h4 class="name-flowers text-center mt-3">${flovers.name}
-       <h4 class="description-flovers text-center">
-           ${flovers.flovers}
-        </h4>
-       <h4 class="cost text-center m-2">${price}</h4>
-       <button class="button-s4">В кошик</button>
-    </div>
- </div>  
+    <div class="card-section4" >
+    <img src="url(${flovers.data.image})" width="310px" height="310px">
+    <h4 class="name-flowers text-center mt-3">${flovers.data.name}
+    </h4>
+    <h4 class="description-flovers text-center">
+        ${flovers.data.flovers}
+    </h4>
+    <h4 class="cost text-center m-2">${flovers.data.price}</h4>
+    <button class="button-s4">В кошик</button>
+
+</div>
+</div>  
         `;
     })
     floversContent = floversContent = `</section>`;
@@ -30,7 +32,12 @@ function displayCardFlovers (){
 }
 
 
-
+function getFlovers (){
+    let apiKey = "20221006140459";
+    let apiUrl = "https://run.mocky.io/v3/275cc213-7485-4396-b1ba-c1969194d67f";
+    axios.get(apiUrl).then(displayCardFlovers(flovers))
+}
+// getFlovers ();
 
 
 
@@ -45,13 +52,13 @@ function displayCardFlovers (){
 //     const cardFlovers = `
 //     <div class="card-of-flowers">
 //     <div class="card-section4" >
-//         <img src="url(${flovers.image})" width="310px" height="310px">
-//         <h4 class="name-flowers text-center mt-3">${flovers.name}
+//         <img src="url(${flovers.data.image})" width="310px" height="310px">
+//         <h4 class="name-flowers text-center mt-3">${flovers.data.name}
 //         </h4>
 //         <h4 class="description-flovers text-center">
-//             ${flovers.flovers}
+//             ${flovers.data.flovers}
 //         </h4>
-//         <h4 class="cost text-center m-2">${price}</h4>
+//         <h4 class="cost text-center m-2">${flovers.data.price}</h4>
 //         <button class="button-s4">В кошик</button>
 
 //     </div>
@@ -60,11 +67,11 @@ function displayCardFlovers (){
 //     return createFragmentTemplate(cardFlovers);
 // }
 
-// function getFlovers (info){
+// function getFlovers (responce){
 //     let apiKey = "20221006140459";
 //     let apiUrl = "https://run.mocky.io/v3/275cc213-7485-4396-b1ba-c1969194d67f";
-//     axios.get(apiUrl).then(createContentTemplate(flovers))
-// }
+//     axios.get(apiUrl).then(createContentTemplate)
+// };
 // getFlovers ();
 
 
