2d66d1eefa1e7b3c2a2736c0b356d8ef6558e191
tasrad <108927043+tasrad@users.noreply.github.com>
2022-10-09 14:14:06 +0300

    added one vsriable code

--- app.js
+++ app.js
@@ -5,38 +5,38 @@
 //     cost: '445'
 // }];
 
-function displayCardFlovers (flovers){
-    let contentFlovers = document.querySelector("#content");
-    let floversContent = `<section id="content">`;
-    flovers.forEach(function(flovers){
-        floversContent = floversContent +
-        `
-        <div class="card-of-flowers">
-    <div class="card-section4" >
-    <img src="url(${flovers.data.image})" width="310px" height="310px">
-    <h4 class="name-flowers text-center mt-3">${flovers.data.name}
-    </h4>
-    <h4 class="description-flovers text-center">
-        ${flovers.data.flovers}
-    </h4>
-    <h4 class="cost text-center m-2">${flovers.data.price}</h4>
-    <button class="button-s4">В кошик</button>
+// function displayCardFlovers (flovers){
+//     let contentFlovers = document.querySelector("#content");
+//     let floversContent = `<section id="content">`;
+//     flovers.forEach(function(flovers){
+//         floversContent = floversContent +
+//         `
+//         <div class="card-of-flowers">
+//     <div class="card-section4" >
+//     <img src="url(${flovers.data.image})" width="310px" height="310px">
+//     <h4 class="name-flowers text-center mt-3">${flovers.data.name}
+//     </h4>
+//     <h4 class="description-flovers text-center">
+//         ${flovers.data.flovers}
+//     </h4>
+//     <h4 class="cost text-center m-2">${flovers.data.price}</h4>
+//     <button class="button-s4">В кошик</button>
 
-</div>
-</div>  
-        `;
-    })
-    floversContent = floversContent = `</section>`;
-    contentFlovers.innerHTML = contentFlovers;
+// </div>
+// </div>  
+//         `;
+//     })
+//     floversContent = floversContent = `</section>`;
+//     contentFlovers.innerHTML = contentFlovers;
 
-}
+// }
 
 
-function getFlovers (){
-    let apiKey = "20221006140459";
-    let apiUrl = "https://run.mocky.io/v3/275cc213-7485-4396-b1ba-c1969194d67f";
-    axios.get(apiUrl).then(displayCardFlovers(flovers))
-}
+// function getFlovers (){
+//     let apiKey = "20221006140459";
+//     let apiUrl = "https://run.mocky.io/v3/275cc213-7485-4396-b1ba-c1969194d67f";
+//     axios.get(apiUrl).then(displayCardFlovers(flovers))
+// }
 // getFlovers ();
 
 
@@ -98,3 +98,36 @@ function getFlovers (){
 
  
 
+function displayCardFlovers (flovers){
+    let contentFlovers = document.querySelector("#content-conteiner");
+    let floversContent = `<section id="content">`;
+    flovers.data.forEach(function(item){
+        floversContent = floversContent +
+        `
+        <div class="card-of-flowers">
+    <div class="card-section4" >
+    <img src="url(${item.image})" width="310px" height="310px">
+    <h4 class="name-flowers text-center mt-3">${item.name}
+    </h4>
+    <h4 class="description-flovers text-center">
+        ${item.flovers}
+    </h4>
+    <h4 class="cost text-center m-2">${item.price}</h4>
+    <button class="button-s4">В кошик</button>
+​
+</div>
+</div>  
+        `;
+    })
+    floversContent = floversContent + `</section>`;
+    contentFlovers.innerHTML = contentFlovers;
+​
+}
+​
+​
+function getFlovers (){
+    let apiKey = "20221006140459";
+    let apiUrl = "https://run.mocky.io/v3/275cc213-7485-4396-b1ba-c1969194d67f";
+    axios.get(apiUrl).then((flovers) => displayCardFlovers(flovers))
+}
+getFlovers ();
